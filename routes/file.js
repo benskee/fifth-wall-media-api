0a70376f7e91f6814c1feb6f9570af36b33d09a4
benskee <benskee33@gmail.com>
2022-01-11 21:26:19 -0500

    readme

--- routes/file.js
+++ routes/file.js
@@ -1,39 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const File = require('../models/File');
-const fs = require('fs');
-const path = require('path');
 const multer = require('multer');
 var cors = require('cors');
 router.use(cors());
 
-
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'src/uploads');
-    },
-    filename: function (req, file, cb) {
-        cb(null, "uploadedFile.json");
-    }
-});
-
 const upload = multer();
-// const upload = multer({ storage: storage }).single("file");
-
-router.delete('/delete', function (req, res) {
-    const directory = './src/uploads';
-
-    fs.readdirSync(directory, (err, files) => {
-        if (err) throw err;
-        for (const file of files) {
-            fs.unlink(path.join(directory, file), err => {
-                if (err) throw err;
-            });
-        }
-    });
-    return res.status(200).send('success');
-
-});
 
 router.post('/upload', upload.any(), async (req, res) => {
         try {
@@ -55,34 +27,6 @@ router.post('/upload', upload.any(), async (req, res) => {
         }
     return res.status(200).send('success');
 });
-
-// router.post('/upload', function (req, res) {
-//     upload(req, res, async function (err) {
-//         if (err instanceof multer.MulterError) {
-//             return res.status(500).json(err);
-//         } else if (err) {
-//             return res.status(500).json(err);
-//         }
-//         try {
-//             const userInput = JSON.parse(req.body.userInput);
-//             const fileJSON = require('../src/uploads/uploadedFile.json');
-//             const newFile = new File({
-//                 body: fileJSON,
-//                 mediaURL: userInput.mediaURL,
-//                 username: userInput.username,
-//                 projectName: userInput.projectName,
-//                 projectType: userInput.projectType,
-//                 timeAdjust: parseInt(userInput.timeAdjust),
-//                 interval: parseInt(userInput.interval)
-//             });
-//             await newFile.save();
-//         } catch (err) {
-//             console.error(err.message);
-//             res.status(500).send('Server Error');
-//         }
-//     });
-//     return res.status(200).send('success');
-// });
 
 router.get('/:id', async (req, res) => {
     try {
